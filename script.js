823c2cde85389cc1923969c58d229d626c6bc5a4
stupidCoder-arunsks <arunsks57@gmail.com>
2022-04-14 20:17:37 +0530

    task 3 completed

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
+
+async function getSignUpInfo(e) {
+
+    console.log("getting frontend signup >>> ");
+
+    e.preventDefault();
+    const userName = document.getElementById('signupUserName').value;
+    const userEmail = document.getElementById('signupEmail').value;
+    const userPhNo = document.getElementById('signupUserPhNo').value;
+    const userPassword = document.getElementById('signupPassword').value;
+
+    console.log('signup details', userName, userEmail, userPhNo, userPassword);
+
+    try {
+        const res = await axios.post("http://localhost:3000/signup", {
+            "name": userName,
+            "email": userEmail,
+            "phoneno": userPhNo,
+            "password": userPassword
+        });
+
+        console.log('res from backend ', res);
+        console.log('res from backend ', res.status);
+
+        if (res.status === 200) {
+            alert("user created successfulyy");
+        }
+        // else if (res.status === 400) {
+        //     alert("Invalid credentials!");
+        // } else {
+        //     console.log(res);
+        // }
+
+    }
+    catch (e) {
+        // alert(`${resMessage}`);
+        alert("User already exists")
+    }
+
+
+}
+
+
 var x = document.getElementById("login");
 var y = document.getElementById("signup");
 var z = document.getElementById("btn");
 
 function signup() {
-    x.style.display="none";
-    y.style.display="block";
+    x.style.display = "none";
+    y.style.display = "block";
     x.style.left = "-400px";
     y.style.left = "50px";
     z.style.left = "110px";
 }
 
 function login() {
-    // x.style.background= "linear-gradient(to right,#ff105f,#ffad06)";
-
-    x.style.display="block";
-    y.style.display="none";
+    x.style.display = "block";
+    y.style.display = "none";
     x.style.left = "50px";
     y.style.left = "450px";
     z.style.left = "0";
